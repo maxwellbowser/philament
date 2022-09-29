c4905142b4797066b1ace24fc8482c90866a693a
Panchbowz <panchobowz@gmail.com>
2022-09-28 21:18:20 -0700

    Writing next weeks email I figured out how to do links, as well as pictures. Also i added a random rick-roll feature lol, so 8% of the time you get rick rolled with the link

--- Chicobot2.py
+++ Chicobot2.py
@@ -1,3 +1,4 @@
+import numbers
 import yagmail
 import keyring
 import pickle
@@ -21,6 +22,14 @@ cities = temp_var2.split(' ')
 cities.pop()
 randomnum = random.randint(0,len(cities))
 
+numbers = [0, 1]
+WeeklyLink = 'https://www.tiktok.com/@ricowright83/video/7132354256476048686?is_from_webapp=1&sender_device=pc'
+
+x = random.choices(numbers, weights = (92, 8))
+
+if x == [1]:
+    WeeklyLink = 'https://www.tiktok.com/@sock.my.this.nutz/video/7032330308221504795?is_from_webapp=1&sender_device=pc&web_id=7148631246494500394'
+
 
 
 headers = {
@@ -43,7 +52,6 @@ city = cities[randomnum]
 city = city+" weather"
 Weather_info = weather(city)
 
-print(Weather_info)
 
 todaysDate = date.today()
 MonthDay = str(todaysDate)[5:]
@@ -58,32 +66,41 @@ else:
     remark = 'Man thats chilly! I hope Ryan puts some hot cocoa in my bowl!'
 
 
+Greeting = f"""Good Morning {EmailList[1][1]}!
+    """
+
 Body = f''' 
-    I hope you're having a good week! Ryan finally got me a computer box, so I could start sending you emails, and man it's hard to type with these paws! There's no way I'm going anywhere near that mouse though, I'm nothing like those cats! 
+    I hope you're having a great week so far! I got to bark at a cat today and Ryan took me for a walk, so I've got no complaints with my life.
 
-    Anyways, Ryan said I should try and help your day go a little smoother, so I found some useful tips. He never told me what city we live in, so I'm just gonna guess every week, let me know if I'm getting close!
+    I'm still learning how to use this internet, and it's so cool, did you know that people take funny videos of dogs?? <a href={WeeklyLink}>Check out this one!</a> 
 
-    I found out that today in {Weather_info[0]}, it's gonna be {Weather_info[3]} degrees, and {Weather_info[2].lower()} outside! {remark}
-
+    Still figuring out where I live but I'm pretty sure we're in the USA. Today in {Weather_info[0]}, it's gonna be {Weather_info[3]} degrees, and {Weather_info[2].lower()} outside! {remark}
 '''
 
 
 Salutation = """   
-Good luck with the rest of the week, I know you're gonna do great!
+Sending lots of love, I hope you're feeling it!
+
+-Chico :)
 
 
--Chico
+p.s. Hey guys, this is Ryan. I asked Chico if I could start putting pictures of him in his emails and he was alright with it so here ya go, enjoy!
              """
 
-
+message = Greeting + Body + Salutation
 
 yag = yagmail.SMTP(user='mr.secretarychico@gmail.com', password=chico_password)
+yag.send(to=EmailList[1][0], subject=f'Chico\'s Check-In {MonthDay}', contents=message, attachments='Chico-picture.jpeg')
+
 
 # sending the email
+'''
 for i in range (0,len(EmailList)):
     
     Greeting = f"""Good Morning {EmailList[i][1]}!
     """
     message = Greeting + Body + Salutation
-    yag.send(to=EmailList[i][0], subject=f'Chico\'s Check-In {MonthDay}', contents=message)
+    yag.send(to=EmailList[i][0], subject=f'Chico\'s Check-In {MonthDay}', contents=message, attachments='Chico-picture.jpeg')
     print(f'Sent to {EmailList[i][1]}')
+
+'''
